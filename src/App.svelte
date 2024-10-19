550af1e22230a42dd64a8f73bbacb8ea80686cd4
Michael Hedgpeth <michael@hedge-ops.com>
2024-10-19 09:37:35 -0600

    Added back vite and svelte logos

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,18 @@
 <script lang="ts">
+  import svelteLogo from "./assets/svelte.svg";
+  import viteLogo from "/vite.svg";
   import Counter from "$lib/components/Counter.svelte";
 </script>
 
+<div class="flex flex-col gap-2 md:flex-row">
+  <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+    <img src={viteLogo} class="logo" alt="Vite Logo" />
+  </a>
+  <a href="https://svelte.dev" target="_blank" rel="noreferrer">
+    <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+  </a>
+</div>
+<h1 class="text-xl">Vite + Svelte</h1>
 <main>
   <Counter />
 </main>
